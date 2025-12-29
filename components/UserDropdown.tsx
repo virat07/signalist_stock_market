261c9ce7703c53b9e2db70e50a45ff42ccb530ab
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2025-12-29 15:03:54 -0800

    removed avatar image

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -28,7 +28,7 @@ const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: Stock
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+                      
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                             {user.name[0]}
                         </AvatarFallback>
